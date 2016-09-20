e85b4714ada3c0e31c472bad22f813d5896d960f
Devon Latzen <d_latzen@yahoo.com>
2016-09-20 10:12:52 -0700

    Improving functionality and animations
    Exploring how to resize fonts. Laying groundwork for split
    functionality.

--- Tip calculator 2/ViewController.swift
+++ Tip calculator 2/ViewController.swift
@@ -28,6 +28,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var theWordsTotalBill: UILabel!
     
+    @IBOutlet weak var splitTab: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -37,6 +40,7 @@ class ViewController: UIViewController {
         billField.frame = CGRect(x: 26, y: 190, width: 173, height: 30)
         barView.frame = CGRect(x: 20, y: 240, width: 280, height: 1)
         barView.alpha = 0
+        splitTab.alpha = 0
         
         // Results group
         totalLabel.textColor = UIColor(red: 50/255, green: 188/255, blue: 50/255, alpha: 1)
@@ -57,22 +61,6 @@ class ViewController: UIViewController {
     }
     
     
-   // internal func fade() {
-   //     UIView.animateWithDuration(1, animations: {
-   //         self.barView.alpha = 1
-   //     })
-   // }
-    
-    
-    internal func moveUp() {
-        UIView.animateWithDuration(0.5, animations: {
-            self.barView.center.y -= 60
-        }) { (_) -> Void in
-            print ("moveUp executed")
-        }
-    }
-    
-    
     internal func expand() {
         UIView.animateWithDuration(0.5, animations: { () -> Void in
             self.barView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
@@ -90,14 +78,15 @@ class ViewController: UIViewController {
         }
     }
     
-    
-    internal func changeColor() {
-        UIView.animateWithDuration(2, delay: 0.6, options: [], animations: {
-            self.barView.backgroundColor = UIColor.redColor()
-        }) { (_) in
-            print ("changeColor executed")
+
+    internal func splitFadeIn() {
+        UIView.animateWithDuration(0.3, delay: 0.1, options: [], animations: {
+            self.splitTab.alpha = 1
+            }) { (_) in
+                print ("splitFadeIn executed")
         }
     }
+    
     
     
     @IBAction func buttonFade(sender: AnyObject) {
@@ -107,6 +96,9 @@ class ViewController: UIViewController {
             self.billField.frame = CGRect(x: 26, y: 77, width: 173, height: 30)
             self.barView.frame = CGRect(x: 20, y: 114, width: 280, height: 1)
             self.barView.alpha = 1
+            self.splitFadeIn()
+//            self.adjustBillFont()
+            
             
             // Results group
             self.tipLabel.alpha = 1
@@ -123,6 +115,13 @@ class ViewController: UIViewController {
     }
     
     
+//    internal func adjustBillFont() {
+//        // Trying to figure out how to resize a textField font
+//        billField.font? = billField.font?.fontWithSize(50)
+//        print ("adjustBillFont loaded")
+//    }
+    
+    
     @IBAction func calculateTip(sender: AnyObject) {
         
         let tipPercentages = [0.18, 0.2, 0.25]
@@ -136,3 +135,32 @@ class ViewController: UIViewController {
         
     }
 }
+
+
+
+// internal func fade() {
+//     UIView.animateWithDuration(1, animations: {
+//         self.barView.alpha = 1
+//     })
+// }
+
+
+
+// internal func moveUp() {
+//     UIView.animateWithDuration(0.5, animations: {
+//         self.barView.center.y -= 60
+//     }) { (_) -> Void in
+//         print ("moveUp executed")
+//     }
+// }
+
+
+
+//    internal func changeColor() {
+//        UIView.animateWithDuration(2, delay: 0.6, options: [], animations: {
+//            self.barView.backgroundColor = UIColor.redColor()
+//        }) { (_) in
+//            print ("changeColor executed")
+//        }
+//    }
+    
